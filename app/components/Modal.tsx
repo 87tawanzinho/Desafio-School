763faa67db44d3a45028c3b46f5cab412cdbf965
tawan <87tawanzinho@gmail.com>
2023-12-30 19:59:11 -0300

    fixing now

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,7 +1,7 @@
 import conversionSemester from "../strings/Semester";
 import Subjects from "./Subjects";
 
-export default function Modal({ setOpen, semester, axiosGet }: ModalI) {
+export default function Modal({ setOpen, semester }: ModalI) {
   return (
     <div className="fixed top-0 left-0 flex justify-center items-center w-full h-full z-50 bg-opacity-75 p-4 bg-zinc-900">
       <div className="modal w-auto   h-auto p-8 lg:p-14 rounded ">
@@ -14,7 +14,7 @@ export default function Modal({ setOpen, semester, axiosGet }: ModalI) {
             X
           </p>
         </div>
-        <Subjects semester={semester} axiosGet={axiosGet} />
+        <Subjects semester={semester} />
       </div>
     </div>
   );
