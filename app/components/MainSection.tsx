4823c492419729348a0f7ca1c61651cc45ec7af5
tawan <87tawanzinho@gmail.com>
2024-01-01 11:49:26 -0300

    header

--- app/components/MainSection.tsx
+++ app/components/MainSection.tsx
@@ -3,7 +3,7 @@ import TakeGrades from "./TakeGrades";
 export default function MainSection() {
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col  px-10 py-10 lg:p-24  w-full  lg:w-9/12 ">
+      <div className="flex flex-col  px-10 py-10 lg:p-24  w-full  lg:w-9/12 mt-20 ">
         <TakeGrades />
       </div>
     </div>
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useEffect, useState } from "react";
+import GraphicsSVG from "./GraphicsSvg";
+
+export default function Navbar() {
+  const [changeColor, setChangeColor] = useState(false);
+
+  const changeBackground = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY >= 4) {
+        setChangeColor(true);
+      } else {
+        setChangeColor(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
+  return (
+    <header
+      className={` ${
+        changeColor ? "text-black bg-gray-100" : "text-white bg-black"
+      } w-full fixed p-4 z-50 items-center  flex text-sm  font-bold transition-all`}
+    >
+      <div
+        className={`flex gap-2 items-center ${
+          changeColor ? "text-violet-900" : "text-yellow-300"
+        }`}
+      >
+        <p className="">Sistema Intelecto </p>
+        <GraphicsSVG />
+      </div>
+      <div className="absolute end-4 flex gap-4">
+        <a href="https://github.com/87tawanzinho" target="_blank">
+          Github
+        </a>
+
+        <a href="https://wa.link/1hcale" target="_blank">
+          Whatsapp
+        </a>
+      </div>
+    </header>
+  );
+}
